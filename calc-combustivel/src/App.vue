1810f562c94d3a299b2a4177500b7d23b472fd4e
Ligia Brusamolin <librusamolin@gmail.com>
2021-03-12 13:47:00 -0300

    Vue e Vuetify instalados. Tela e inputs desenvolvidos.

---
+++ calc-combustivel/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <InputGas/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import InputGas from './components/InputGas';
+
+export default {
+  name: 'App',
+
+  components: {
+    InputGas,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ calc-combustivel/src/components/InputGas.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="input-gas">
+    <h2>Calculadora de Combustível</h2>
+    <h3>Insira os valores do Etanol e Gasolina, para calcular qual é mais vantajoso.</h3>
+    <form>
+        <v-text-field
+            label="Valor do Etanol"
+            outlined
+        ></v-text-field>
+        <v-text-field
+            label="Valor da Gasolina"
+            outlined
+        ></v-text-field>
+         <v-btn
+            class="mr-4"
+            type="submit"
+            :disabled="invalid"
+        >
+            enviar
+        </v-btn>
+        <v-btn @click="clear">
+            limpar
+        </v-btn>
+    </form>
+</div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    h2, h3 {
+        font-weight: 300;
+    }
+    h2 {
+        margin-top: 20px;
+    }
+    .input-gas {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+    form {
+        padding-top: 20px;
+    }
+</style>
